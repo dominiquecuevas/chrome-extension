449d5470f364741560ae0a62ed79f0c77701a353
Dominique Cuevas <cuevasdominique@gmail.com>
2020-08-09 21:30:38 -0700

    style sheet to change background-color

--- darkish-mode/background.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#6699cc'}, function() {
-        console.log("This is your color now.");
-    })
-})
